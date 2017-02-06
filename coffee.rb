a4ca72e736351c85ac07e44f957d2988688718c1
Nancy Ingels <ingelsna@gmail.com>
2017-02-06 16:23:21 -0500

    pushing my copy

--- coffee.rb
+++ coffee.rb
@@ -1,3 +1,31 @@
 class Coffee
-  
+
+    attr_accessor :coffee_name,
+                  :coffee_level,
+                  :coffee_potency
+
+    def initialize(coffee_name)
+      self.coffee_name = coffee_name
+      if coffee_name == "Triple Shot Mocha Frappuccino"
+        self.coffee_potency = 0.31
+        self.coffee_level = 3
+      end
+
+    end
+
+#--------------------------------
+
+
+    def full
+      true
+    end
+
+    def empty?
+      coffee_level == 0
+    end
+
+    def full?
+      coffee_level == 3
+    end
+
 end
--- human.rb
+++ human.rb
@@ -1,10 +1,36 @@
 class Human
   attr_accessor :name,
-                :alertness
+                :alertness,
+                :cup
 
-  def initialize(name, alertness=0)
+  def initialize(name, alertness=0, type="", num_drinks=0)
     self.name = name
-    self.alertness = alertness
+    self.alertness = alertness.to_f
+    puts "self #{self.name}"
+    puts self.alertness
+
+  end
+
+  def buy(coffee_name)
+    self.cup = coffee_name
+  end
+
+  def drink!
+    self.alertness = alertness + 0.31
+    self.cup.coffee_level = self.cup.coffee_level - 1
+    puts self.inspect
+    puts self.cup.inspect
+
+
+  end
+
+  def has_coffee?
+    alertness > 0
+  end
+
+  def needs_coffee?
+    alertness <= 0
+
   end
 
 
